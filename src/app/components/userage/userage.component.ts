85cacd87c4cbb87658a566b2c6f89715dfce4a12
jerome09232 <jeromecberiso87@gmail.com>
2024-09-19 01:41:44 +0900

    #6userage

--- src/app/components/userage/userage.component.ts
+++ src/app/components/userage/userage.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './userage.component.css'
 })
 export class UserageComponent {
+  birthYear: number = 0;
+  // @ts-ignore
+  age: number = null;
+  // @ts-ignore
+  age: number | undefined;
 
+  calculateAge() {
+    const currentYear = new Date().getFullYear();
+    this.age = currentYear - this.birthYear;
+  }
 }
