c037a1d5f963d8cf3af12a05a88d61e29adf92b4
jerome09232 <jeromecberiso87@gmail.com>
2024-09-19 01:57:32 +0900

    #11evenoddchecker

--- src/app/components/evenoddchecker/evenoddchecker.component.ts
+++ src/app/components/evenoddchecker/evenoddchecker.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './evenoddchecker.component.css'
 })
 export class EvenoddcheckerComponent {
+  inputNumber: number = 0;
+  result: string = '';
+  // @ts-ignore
+  result: string;
 
+  checkEvenOdd() {
+    if (this.inputNumber % 2 === 0) {
+      this.result = 'Even';
+    } else {
+      this.result = 'Odd';
+    }
+  }
 }
