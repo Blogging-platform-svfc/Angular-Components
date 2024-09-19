16ea6630d983d60d1f3e887be5ce6d4ac7fd0ac9
jerome09232 <jeromecberiso87@gmail.com>
2024-09-19 14:47:18 +0900

    #41namegenerator

--- src/app/components/agegenerator/agegenerator.component.ts
+++ src/app/components/agegenerator/agegenerator.component.ts
@@ -1,10 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-agegenerator',
-  templateUrl: './agegenerator.component.html',
-  styleUrl: './agegenerator.component.css'
-})
-export class AgegeneratorComponent {
-
-}
