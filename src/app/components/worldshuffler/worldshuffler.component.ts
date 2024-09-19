ce0a5f183328de6ea50aef5c98de2ac3b88ad894
jerome09232 <jeromecberiso87@gmail.com>
2024-09-19 12:26:11 +0900

    #30wordshuffler

--- src/app/components/worldshuffler/worldshuffler.component.ts
+++ src/app/components/worldshuffler/worldshuffler.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './worldshuffler.component.css'
 })
 export class WorldshufflerComponent {
+  inputWord: string = '';
+  shuffledWord: string = '';
 
+  shuffleWord() {
+    const letters = this.inputWord.split('');
+    for (let i = letters.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [letters[i], letters[j]] = [letters[j], letters[i]];
+    }
+    this.shuffledWord = letters.join('');
+  }
 }
