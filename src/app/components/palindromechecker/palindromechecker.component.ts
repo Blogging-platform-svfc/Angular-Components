8249941f07303e4fbf819833f6ebed0865070e30
jerome09232 <jeromecberiso87@gmail.com>
2024-09-19 10:33:04 +0900

    #20palindromechecker

--- src/app/components/palindromechecker/palindromechecker.component.ts
+++ src/app/components/palindromechecker/palindromechecker.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-palindromechecker',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './palindromechecker.component.html',
   styleUrl: './palindromechecker.component.css'
 })
 export class PalindromecheckerComponent {
+  inputWord: string = '';
+  isPalindrome: boolean | null = null;
 
+  checkPalindrome() {
+    const cleanedWord = this.inputWord.toLowerCase().replace(/[^a-z]/g, '');
+    this.isPalindrome = cleanedWord === cleanedWord.split('').reverse().join('');
+  }
 }
